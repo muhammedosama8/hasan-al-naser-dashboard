085ad8674e9d89410d2cec8c5a3e930f535534b8
Muhammed Osama <muhammed.o.nasser@gmail.com>
2024-07-15 08:25:16 +0300

    update

--- src/services/MHContactusService.js
+++ src/services/MHContactusService.js
@@ -1,10 +1,16 @@
 import BaseService from "./BaseService";
+import http from './HttpService'
 import { API_BASE_URL_ENV } from "../jsx/common/common";
 
 const apiEndpoint = API_BASE_URL_ENV() + "/setting/contactUs";
+const apiUpdateEndpoint = API_BASE_URL_ENV() + "/setting/closeContactUs";
 
 export default class MHContactusService extends BaseService {
     constructor() {
         super(apiEndpoint);
+    }
+
+    update(id) {
+        return http.put(`${apiUpdateEndpoint}/${id}`);
     }
 }
