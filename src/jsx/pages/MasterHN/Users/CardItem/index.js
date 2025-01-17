b0e2abfa03ffa50e40d1576f1809a539a284b86e
Muhammed Osama <muhammed.o.nasser@gmail.com>
2025-01-17 15:48:04 +0200

    update

--- src/jsx/pages/MasterHN/Users/CardItem/index.js
+++ src/jsx/pages/MasterHN/Users/CardItem/index.js
@@ -1,10 +1,8 @@
 import { useEffect, useState } from "react";
 import { Form } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
 
 const CardItem = ({item, index,setShouldUpdate}) =>{
     const [status, setStatus] = useState(null)
-    const navigate = useNavigate()
 
     useEffect(()=>{
         setStatus(item.isBlocked)
@@ -16,26 +14,23 @@ const CardItem = ({item, index,setShouldUpdate}) =>{
                       <strong>{item.id}</strong>
                     </td>
                     <td>
-                      {item.f_name ? <p  className="mb-0 user" onClick={()=> navigate(`/users/profile`, {state:  item})}
-                        style={{fontWeight: !!item.f_name && '800',opacity:'.75', textDecoration:!!item.f_name &&'underline', textTransform: 'capitalize', cursor: 'pointer' }}>
-                        {item.f_name} {item.l_name} 
+                      {item?.username ? <p className="mb-0"
+                        style={{fontWeight: '800'}}>
+                        {item.username} 
                       </p> : '-'}
                     </td>
                     <td>
                       {item.email || '-'}
                     </td>
-                    <td style={{cursor: 'pointer'}} onClick={()=> {
-                      navigate(`/users/profile`, {state:  item})
-                    }}>
-                      {item?.user_phones?.filter(res=> !!res.is_default)[0]?.country_code}{item?.user_phones?.filter(res=> !!res.is_default)[0]?.phone || '-'}
+                    <td>
+                      {/* {item?.user_phones?.filter(res=> !!res.is_default)[0]?.country_code}{item?.user_phones?.filter(res=> !!res.is_default)[0]?.phone || '-'} */}
+                      {item?.country_code} {item?.phone || '-'}
                     </td>
-                    {/* <td>{item.count_orders || '-'}</td> */}
                     <td>
                       <Form.Check
                         type="switch"
                         id={`custom-switch${index}`}
                         checked={!status}
-                        // onChange={(e)=> changeStatusToggle(e)}
                       />
                     </td>
                     <td>
@@ -43,23 +38,8 @@ const CardItem = ({item, index,setShouldUpdate}) =>{
                         type="switch"
                         id={`isDeleted${index}`}
                         checked={item.isDeleted}
-                        // onChange={(e)=> changeStatusToggle(e)}
                       />
                     </td>
-                    {/* <td>
-                     {isExist('users') && <Dropdown>
-                        <Dropdown.Toggle
-                          // variant="success"
-                          className="light sharp i-false"
-                        >
-                          <i className="la la-ellipsis-v" style={{fontSize: '27px'}}></i>
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>
-                           <Dropdown.Item>Edit</Dropdown.Item> 
-                          <Dropdown.Item>Delete</Dropdown.Item>
-                        </Dropdown.Menu>
-                      </Dropdown>}
-                    </td> */}
                   </tr>
     )
 }
